db9be643fce4c21a17c68ee590265271c58c03b4
cqx820 <673545775@qq.com>
2017-03-19 15:24:04 -0600

    Users done for now

--- src/phase2/Users.java
+++ src/phase2/Users.java
@@ -26,6 +26,7 @@ public class Users {
 		String[] trusted;
 		ArrayList<String[]> array = new ArrayList<String[]>();
 		ResultSet rs = null;
+		int resultCount = 0;
 		if (amount.equals("ALL")) {
 			String sql = "select u.name, u.login, sum(t.isTrusted) AS TotalTrust From Trust t, Users u where t.login2 = u.login"
 					+ " group by u.name, having sum(t.isTrusted) > 0 order by TotalTrust desc";
@@ -37,13 +38,18 @@ public class Users {
 					trusted[1] = rs.getString("login");
 					trusted[2] = rs.getString("TotalTrust");
 					array.add(trusted);
+					resultCount++;
 				}
 				rs.close();
+
 			} catch (SQLException e) {
 				System.out.println(e.getMessage());
 
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
+			} finally {
+				if (resultCount == 0)
+					return null;
 			}
 
 		} else {
@@ -95,33 +101,79 @@ public class Users {
 
 	}
 
-	public ArrayList<String> getUserLogin(String name, Statement st) {
+	public String getUserLogin(String name, String contact_Num, Statement st) {
 		ResultSet rs = null;
-		ArrayList<String> login = new ArrayList<String>();
-		String sql = "select login from Users where name = " + name + ";";
+		String login = "";
+		int loginCount = 0;
+
+		// ArrayList<String> login = new ArrayList<String>();
+		String sql = "select login from Users where name = " + name + " AND contact_Num = " + contact_Num + ";";
 		try {
 			rs = st.executeQuery(sql);
 			while (rs.next()) {
-				login.add(rs.getString("login"));
+				// login.add(rs.getString("login"));
+				login = rs.getString("login");
+				loginCount++;
 			}
+			if (loginCount == 0)
+				return ""; // No result founded
 			rs.close();
-			if (login.size() < 1)
-				return null;
+			// if (login.size() < 1)
+			// return null;
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		// finally
-		// {
-		// if(st == null)
-		// {
-		//
-		// }
+		// finally {
+		// if(loginCount == 0) return ""; // No result founded
 		// }
 
 		return login;
 
 	}
 
+	public ArrayList<String> getOneDegreeSeperation(String nameA, String contact_Num, Statement st) {
+		String nameALogin = getUserLogin(nameA, contact_Num, st);
+		ArrayList<String> forReturn = new ArrayList<String>();
+
+		// select u.login from Users u, Favorites f1 where exists(select
+		// f2.login from Favorites f2 where f1.login != f2.login and f1.hid =
+		// f2.hid);
+		String sql = "select u.name from Users u, Favorites f1 where exists (select f2.login from Favorites where "
+				+ nameALogin + " = f1.login and f1.login != f2.login and f1.hid = f2.hid;";
+		ResultSet rs = null;
+		try {
+			rs = st.executeQuery(sql);
+			while (rs.next()) {
+				forReturn.add(rs.getString("name"));
+			}
+			if (forReturn.isEmpty())
+				return forReturn;
+			rs.close();
+		} catch (SQLException e) {
+
+		} catch (Exception e) {
+
+		}
+
+		return forReturn;
+	}
+
+	public ArrayList<String> getTwoDegreeSeperation(String userNameA, String userNameB, String contact_NumA,
+			String contact_NumB, Statement st) {
+		ArrayList<String> A_oneDegreeList = getOneDegreeSeperation(userNameA, contact_NumA, st);
+		ArrayList<String> B_oneDegreeList = getOneDegreeSeperation(userNameB, contact_NumB, st);
+		ArrayList<String> forReturn = new ArrayList<String>();
+		if(A_oneDegreeList.isEmpty() || B_oneDegreeList.isEmpty()) return forReturn;
+	//	else if(A_oneDegreeList.contains(userNameB) || B_oneDegreeList.contains(userNameA)) return forReturn;
+		else if(!A_oneDegreeList.contains(userNameB) && !B_oneDegreeList.contains(userNameA))
+		{
+			for(String s : A_oneDegreeList)
+			{
+				if(B_oneDegreeList.contains(s)) forReturn.add(s);
+			}
+		}
+		return forReturn;
+	}
 }
