4b0796f2040c6cf783ddad4b9a63b83c16a43b96
cqx820 <673545775@qq.com>
2017-03-21 14:29:02 -0600

    MERGE FEEDBACK

--- src/phase2/Feedback.java
+++ src/phase2/Feedback.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 public class Feedback {
 
 	public boolean giveFeedback(String h_id, String login, String text, String score, String date, Statement stmt) {
-
-		// new connection??????
-		if (!checkGive(h_id, login, stmt)) {
+		
+		//new connection??????
+		if(!checkGive(h_id,login,stmt)){
 			return false;
 		}
 		String sql = "insert into Feedback (h_id, login, text, score, fbdate) " + "VALUES ('" + h_id + "','" + login
@@ -29,10 +29,11 @@ public class Feedback {
 		}
 		return false;
 	}
+	
+private boolean checkGive(String h_id, String login, Statement stmt) {
+		
 
-	private boolean checkGive(String h_id, String login, Statement stmt) {
-
-		String sql = "select * from Feedback  where h_id= " + h_id + " and login= " + login + ";";
+		String sql = "select * from Feedback  where h_id= " +h_id+" and login= " + login +";";
 
 		int rs = 0;
 
@@ -49,78 +50,88 @@ public class Feedback {
 		}
 		return false;
 	}
-
+	
 	public ArrayList<String> getTHFeedback(String h_id, String amount, Statement stmt) {
-		ArrayList<String> result = new ArrayList<String>();
-
+		ArrayList<String> result = new ArrayList<String> ();
+		
+		
 		// Rating query
-		if (amount.equals("all")) {
-			String sql = "select h_id, text, score from Feedback " + "where h_id ='" + h_id + "';";
-
+		if(amount.equals("all")) {
+			String sql="select distinct f.text, f.score from Rates r, Feedback f"
+					+    " where f.fid IN (select AVG(rating) as average,f_id from "
+					+ "Feedback where h_id = '" +h_id +"')"
+					+    " order by average" + "';";
+			
 			ResultSet rs = null;
-			try {
-				rs = stmt.executeQuery(sql);
-				while (rs.next()) {
-					String temp = "";
-					temp = rs.getString("text") + "\t" + rs.getString("score");
-					result.add(temp);
-				}
-				return result;
-			} catch (Exception e) {
-
-				System.err.println(e.getMessage());
-			}
+		 	try {
+		 		rs = stmt.executeQuery(sql);
+		 		while (rs.next()) {
+		 			String temp="";
+		 			temp = rs.getString("text") + "\t" + rs.getString("score");
+		 			result.add(temp);
+		 		}
+		 		return result;
+		 	}
+		 	catch(Exception e) {
+		 		
+		 		System.err.println(e.getMessage());
+		 	}		
 			return result;
-		} else {
+		}
+		else {
 			int count = Integer.parseInt(amount);
-
 			String sql = "select distinct f.text, f.score from Rates r, Feedback f"
-					+ " where f.fid IN (select fid from Feedback where h_id = '" + h_id + "')" + " order by r.rating"
-					+ " limit " + count + ";";
-
+					+    " where f.fid IN (select AVG(rating) as average,f_id from "
+					+ "Feedback where h_id = '" +h_id +"')"
+					+    " order by average"
+					+    " limit " +count +";";
+			
 			ResultSet rs = null;
-			// System.out.println("executing "+sql);
-			try {
-				rs = stmt.executeQuery(sql);
-				while (rs.next()) {
-					String temp;
-					temp = rs.getString("text") + "\t" + rs.getString("score");
-					result.add(temp);
-				}
-				return result;
-			} catch (Exception e) {
-				System.err.println(e.getMessage());
-			}
-
+//			System.out.println("executing "+sql);
+		 	try {
+		 		rs = stmt.executeQuery(sql);
+		 		while (rs.next()) {
+		 			String temp;
+		 			temp = rs.getString("text") + "\t" + rs.getString("score");		 			
+		 			result.add(temp);
+		 		}
+		 		return result;
+		 	}
+		 	catch(Exception e) { 		
+		 		System.err.println(e.getMessage());
+		 	}
+		
 			return result;
 		}
 	}
-
 	public boolean rateFeedback(String login, int f_id, String rating, Statement stmt) {
-
-		if (!checkRate(f_id, login, stmt)) {
+		
+		if(!checkRate(f_id, login, stmt)){
 			return false;
 		}
-		String sql = "INSERT INTO Rates (login, f_id, rating) " + "VALUES ('" + login + "','" + f_id + "','" + rating
-				+ "');";
+		String sql="INSERT INTO Rates (login, f_id, rating) "
+				+  "VALUES ('" +login +"','" +f_id +"','" +rating +"');";
 		int rs = 0;
-		try {
-			rs = stmt.executeUpdate(sql);
-
-			if (rs > 0) {
-				return true;
-			} else {
-				return false;
-			}
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
+	 	try {
+	 		rs = stmt.executeUpdate(sql);
+	 		
+	 		if(rs > 0) {
+	 			return true;
+	 		}
+	 		else {
+	 			return false;
+	 		}
+	 	}
+	 	catch(Exception e) {
+	 		System.err.println(e.getMessage());
+	 	}
 		return false;
 	}
+	private boolean checkRate(int f_id,String login, Statement stmt) {
+		
 
-	private boolean checkRate(int f_id, String login, Statement stmt) {
-
-		String sql = "select * from Rates r, Feedback f  where f.login= " + login + " and f.f_id =" + f_id + ";";
+		String sql = "select * from Rates r, Feedback f  where f.login= " 
+		+login+ " and f.f_id =" +f_id+ ";";
 
 		int rs = 0;
 
